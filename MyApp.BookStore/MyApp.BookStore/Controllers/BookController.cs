56594f8dcc31a5ed25c535e1b01ec13bf236e2c9
ketan  funde <kfunde@capritechnosys.com>
2024-12-30 12:09:51 +0530

    basic idea of working with model and controller

---
+++ MyApp.BookStore/MyApp.BookStore/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.BookStore.Models;
+using MyApp.BookStore.Repository;
+using System.Collections.Generic;
+
+namespace MyApp.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();           
+        }
+
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string author)
+        {
+            return _bookRepository.SearchBooks(bookName, author);
+        }
+    }
+}
