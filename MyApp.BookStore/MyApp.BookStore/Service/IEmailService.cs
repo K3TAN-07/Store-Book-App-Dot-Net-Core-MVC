958751267347cfa5ec341de566372017b1796b9e
ketan  funde <kfunde@capritechnosys.com>
2025-01-07 17:04:24 +0530

    SMTP email feature

---
+++ MyApp.BookStore/MyApp.BookStore/Service/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using MyApp.BookStore.Models;
+using System.Threading.Tasks;
+
+namespace MyApp.BookStore.Service
+{
+    public interface IEmailService
+    {
+        Task SendTestEmail(UserEmailOptions userEmailOptions);
+    }
+}
--- MyApp.BookStore/MyApp.BookStore/Startup.cs
+++ MyApp.BookStore/MyApp.BookStore/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MyApp.BookStore.Data;
@@ -17,7 +18,14 @@ using System.Threading.Tasks;
 namespace MyApp.BookStore
 {
     public class Startup
+
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -40,6 +48,8 @@ namespace MyApp.BookStore
             services.AddScoped<IAccoountRepository, AccoountRepository>();
             services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, ApplicationUserClaimsPrincipalFactory>();
             services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IEmailService, EmailService>();
+            services.Configure<SMTPConfigModel>(_configuration.GetSection("SMTPConfig"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
