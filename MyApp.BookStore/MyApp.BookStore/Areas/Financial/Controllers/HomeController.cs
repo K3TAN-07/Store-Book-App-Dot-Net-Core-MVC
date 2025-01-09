1499535e73cf242ae2fcdc61026d4a0a6ccad127
ketan  funde <kfunde@capritechnosys.com>
2025-01-09 17:07:26 +0530

    areas in asp.net core

---
+++ MyApp.BookStore/MyApp.BookStore/Areas/Financial/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.BookStore.Areas.Financial.Controllers
+{
+    [Area("financial")]
+    [Route("financial/[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
